4617f33b89afdefbcdb273ebfc09f274b4a2eae5
Azzamsyakir <azzamsykir@gmail.com>
2025-08-26 10:04:52 +0700

    feat: update server add more modular approach for routes

--- src/server/http/server.js
+++ src/server/http/server.js
@@ -1,19 +1,27 @@
 import env from "#config/env_config";
-import { NewRoutes } from "#httpServer/routes/routes";
+import { RegisterRoutes } from "#httpServer/routes/routes";
 import express from "express"
 // import { NewMiddliware } from "./middlewares/middleware.js";
 
 
 export function newServer() {
-
   const app = express();
-  const host = env.app.APP_HOST;
-  const port = env.app.APP_PORT
+  const host = env.app.APP_HOST || "localhost";
+  const port = env.app.APP_PORT || 3000;
+  const router = express.Router();
+  RegisterRoutes(app, router);
+  app.set("port", port);
 
-  // NewMiddliware();
-  NewRoutes(app);
-
-  app.listen(port, () => {
-    console.log(`App Running at ${host}:${port}`);
+  const server = app.listen(app.get("port"), host, () => {
+    console.log(`Server started on http://${host}:${app.get("port")}`);
   });
+  server.on('error', (err) => {
+    if (err.code === 'EADDRINUSE') {
+      console.error(`Port ${port} is already in use.`);
+    } else {
+      console.error('Server error:', err);
+    }
+    process.exit(1);
+  });
+  return app;
 }
