6d0ae991c61a29d4df7ac391be0b1699d824f87a
Azzamsyakir <azzamsykir@gmail.com>
2025-08-27 14:23:05 +0700

    feat: add transaction dto

--- src/dto/transaction.js
+++ src/dto/transaction.js
@@ -1,6 +1,47 @@
-export class CreateNewTransactionDto {
+export class TransactionDto {
   constructor(amount, paymentMethod) {
-    this.amount = amount
-    this.paymentMethod = paymentMethod
+    this.amount = amount;
+    this.paymentMethod = paymentMethod;
   }
+
+  static fromRequest(body) {
+    return new TransactionDto(body.amount, body.payment_method);
+  }
+
+  hasAllRequiredFields() {
+    return this.amount !== undefined && this.paymentMethod !== undefined;
+  }
+
+  isValidAmount() {
+    return Number.isInteger(this.amount) && this.amount > 0;
+  }
+
+  isValidPaymentMethod() {
+    const allowed = ['card', 'bank_direct', 'alipay'];
+    return allowed.includes(this.paymentMethod);
+  }
+  validate() {
+    const errors = [];
+
+    if (this.amount === undefined) {
+      errors.push("amount is required");
+    } else if (!this.isValidAmount()) {
+      errors.push("amount must be an integer greater than 0");
+    }
+
+    if (this.paymentMethod === undefined) {
+      errors.push("payment_method is required");
+    } else if (!this.isValidPaymentMethod()) {
+      errors.push("payment_method must be one of: card, direct_debit, alipay");
+    }
+
+    const valid = errors.length === 0;
+
+    return {
+      valid,
+      message: valid ? null : errors.join(", ")
+    };
+  }
+
+
 }
