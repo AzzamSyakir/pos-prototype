38e2f617e6372f0a6df49420dbe2054f2e60efbe
Azzamsyakir <azzamsykir@gmail.com>
2025-08-27 14:23:23 +0700

    feat: update transaction services

--- src/services/transaction.js
+++ src/services/transaction.js
@@ -1,6 +1,13 @@
 import { TransactionEntity } from '#entity/transaction'
-
-export function CreateTransaction(amount) {
-  const trx = new TransactionEntity(amount);
-  console.log(trx);
+import * as paymentGatewayUtils from '#utils/payment_gateway_utils'
+/**
+ * @param {import('#dto/transaction').TransactionDto} dto
+ */
+export async function CreateTransaction(dto) {
+  const trx = new TransactionEntity(
+    dto.amount,
+    dto.paymentMethod
+  );
+  const paymentIntent = await paymentGatewayUtils.CreatePaymentIntent(trx)
+  return paymentIntent
 }
