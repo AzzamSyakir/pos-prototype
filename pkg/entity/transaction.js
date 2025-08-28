2a7b03f8253de355039b5cec68b4a7bd93bd4100
Azzamsyakir <azzamsykir@gmail.com>
2025-08-28 14:19:19 +0700

    fix: fixing up and updating transaction entity

--- pkg/entity/transaction.js
+++ pkg/entity/transaction.js
@@ -1,12 +1,14 @@
 export class TransactionEntity {
-  constructor(amount, paymentMethod, accountholderName, email, accountNumber, routingNumber) {
+  constructor(id, amount, paymentMethod, accountholderName, email, accountNumber, routingNumber) {
+    this.id = id
     this.amount = amount;
     this.status = "pending";
     this.paymentMethod = paymentMethod;
 
-
-    if (paymentMethod === 'ach_direct_debit' && accountholderName) {
+    if (paymentMethod === 'ach_direct_debit' && [accountholderName, accountNumber, routingNumber]) {
       this.accountholderName = accountholderName;
+      this.accountNumber = accountNumber;
+      this.routingNumber = routingNumber;
     }
     if (paymentMethod === 'sepa_debit' && email) {
       this.email = email;
