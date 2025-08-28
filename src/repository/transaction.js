e04117322ff63785605d7eb5c226333e80af2fe0
Azzamsyakir <azzamsykir@gmail.com>
2025-08-28 14:20:17 +0700

    feat: add transaction repository

--- src/repository/transaction.js
+++ src/repository/transaction.js
@@ -1,3 +1,34 @@
-function CreateTransaction() {
+import * as models from '#models/transaction'
+import db from "#config/db_config";
+/**
+ * @param {import('#entity/transaction').TransactionEntity} trx
+ */
+export async function CreateTransaction(trx, currentTime) {
+  const model = new models.TransactionModel(
+    trx.id,
+    trx.amount,
+    trx.status,
+    trx.paymentMethod,
+    currentTime,
+    currentTime
+  );
 
+  const query = `
+    INSERT INTO transactions
+      (id, amount, status, payment_method, created_at, updated_at)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING *;
+  `;
+
+  const values = [
+    model.id,
+    model.amount,
+    model.status,
+    model.paymentMethod,
+    model.created_at,
+    model.updated_at
+  ];
+
+  const result = await db.query(query, values);
+  return result.rows[0];
 }
