5e13fdbe5a98832eea5b50658a06ea353e363519
Azzamsyakir <azzamsykir@gmail.com>
2025-08-28 14:21:08 +0700

    feat: add ccalling to repo in services

--- src/services/transaction.js
+++ src/services/transaction.js
@@ -1,13 +1,21 @@
 import { TransactionEntity } from '#entity/transaction'
 import * as paymentGatewayUtils from '#utils/payment_gateway_utils'
+import * as transactionRepo from '#repository/transaction'
 /**
  * @param {import('#dto/transaction').TransactionDto} dto
  */
 export async function CreateTransaction(dto) {
   const trx = new TransactionEntity(
+    crypto.randomUUID(),
     dto.amount,
-    dto.paymentMethod
+    dto.paymentMethod,
+    dto.accountholderName,
+    dto.email,
+    dto.accountNumber,
+    dto.routingNumber
   );
-  const paymentIntent = await paymentGatewayUtils.CreatePayment(trx)
-  return paymentIntent
+  const currentTime = new Date();
+  const result = transactionRepo.CreateTransaction(trx, currentTime);
+  paymentGatewayUtils.CreatePayment(trx)
+  return result
 }
