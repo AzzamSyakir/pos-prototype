a5417b34ee81287827760a2d563cfe677da8ba90
Azzamsyakir <azzamsykir@gmail.com>
2025-09-01 06:39:55 +0700

    feat: add auths table

--- docker/config/db/migration/init.sql
+++ docker/config/db/migration/init.sql
@@ -1,11 +1,24 @@
 DROP TABLE IF EXISTS transactions;
 
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE
   transactions (
     id UUID PRIMARY KEY,
     amount NUMERIC(15, 2) NOT NULL,
     status VARCHAR(20) CHECK (status IN ('success', 'pending', 'failed')),
     payment_method VARCHAR(20),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+  );
+
+CREATE TABLE
+  users (
+    id UUID PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    stripe_customer_id VARCHAR,
     created_at TIMESTAMP,
     updated_at TIMESTAMP
   );
