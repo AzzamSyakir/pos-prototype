b45124a3d6ac8af826ec0934fb947e7671fb95ca
Azzamsyakir <azzamsykir@gmail.com>
2025-09-09 09:54:14 +0700

    feat: add cors allow all origin

--- src/server/http/routes/routes.js
+++ src/server/http/routes/routes.js
@@ -1,12 +1,13 @@
 import { TransactionRoutes } from './transaction.js';
 import { AuthRoutes } from './auth.js';
 import { WebhookRoutes } from './webhook.js';
+import cors from 'cors';
 /**
  * @param {object} deps
  */
 export function RegisterRoutes(app) {
+  app.use(cors())
   TransactionRoutes(app);
   AuthRoutes(app);
   WebhookRoutes(app);
-
 }
