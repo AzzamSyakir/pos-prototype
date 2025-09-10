48f9430ccc2d7d6a963eb95103372be9c332fe77
Azzamsyakir <azzamsykir@gmail.com>
2025-09-10 11:53:30 +0700

    feat: update transaction models

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -1,10 +1,11 @@
 export class TransactionModel {
-  constructor(id, amount, status, paymentMethod, userId, created_at, updated_at) {
+  constructor(id, amount, status, paymentMethod, userId, stripePaymentId, created_at, updated_at) {
     this.id = id;
     this.amount = amount;
     this.status = status;
     this.paymentMethod = paymentMethod;
     this.userId = userId;
+    this.stripePaymentId = stripePaymentId;
     this.created_at = created_at;
     this.updated_at = updated_at
   }
