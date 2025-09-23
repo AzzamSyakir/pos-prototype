8162fa5360d8e0ede24f42a8bd3d351f106f397c
Azzamsyakir <azzamsykir@gmail.com>
2025-09-23 14:04:50 +0700

    feat: implement and use seed in auth test

--- tests/integration/auth.api.test.js
+++ tests/integration/auth.api.test.js
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { newServer } from '#httpServer';
 const app = newServer();
+import { UserSeeder } from "../seeds/script/seed_users.js";
 
 describe('POST /api/auth/register', () => {
   const endpoint = '/api/auth/register';
@@ -12,26 +13,33 @@ describe('POST /api/auth/register', () => {
       throw err;
     }
   };
-  it('should return success with status code 201 and message "Register success" when all fields are valid', async () => {
-    const res = await request(app).post(endpoint).send({
-      name: "Test User",
-      email: 'user@test.com',
-      password: 'password123',
-      phone_number: '081234567891',
-    });
 
+  const userSeed = new UserSeeder(1);
+  const [userData] = userSeed.userSeedData.data;
+
+  const payload = {
+    name: userData.name,
+    email: userData.email,
+    password: userData.password,
+    phone_number: userData.phoneNumber,
+  };
+
+  afterAll(async () => {
+    await userSeed.Down();
+  });
+
+  it('should return success with status code 201 and message "Register success" when all fields are valid', async () => {
+    const res = await request(app).post(endpoint).send(payload);
+    userData.id = res.body.data.id;
     logOnFail(res, () => expect(res.body.code).toBe(201));
     logOnFail(res, () => expect(res.body).toHaveProperty('data'));
-    logOnFail(res, () => expect(res.body.data.email).toBe('user@test.com'));
+    logOnFail(res, () => expect(res.body.data.email).toBe(userData.email));
     logOnFail(res, () => expect(res.body.message).toBe('Register success'));
   });
 
   it('should return error with status code 400 and message "name is required" when name is missing', async () => {
-    const res = await request(app).post(endpoint).send({
-      email: 'user@test.com',
-      password: 'password123',
-      phone_number: '081234567890',
-    });
+    const { name, ...invalidPayload } = payload;
+    const res = await request(app).post(endpoint).send(invalidPayload);
 
     logOnFail(res, () => expect(res.statusCode).toBe(400));
     logOnFail(res, () => expect(res.body.code).toBe(400));
@@ -40,11 +48,8 @@ describe('POST /api/auth/register', () => {
   });
 
   it('should return error with status code 400 and message "email is required" when email is missing', async () => {
-    const res = await request(app).post(endpoint).send({
-      name: 'Test User',
-      password: 'password123',
-      phone_number: '081234567890',
-    });
+    const { email, ...invalidPayload } = payload;
+    const res = await request(app).post(endpoint).send(invalidPayload);
 
     logOnFail(res, () => expect(res.statusCode).toBe(400));
     logOnFail(res, () => expect(res.body.code).toBe(400));
@@ -53,16 +58,9 @@ describe('POST /api/auth/register', () => {
   });
 
   it('should return error 400 when email already exists', async () => {
-    const payload = {
-      name: 'Test User',
-      email: 'user@test.com',
-      password: 'password123',
-      phone_number: '081234567890',
-    };
-
     const res = await request(app).post(endpoint).send({
       ...payload,
-      name: 'test user',
+      name: 'Another User',
     });
 
     logOnFail(res, () => expect(res.statusCode).toBe(400));
@@ -70,17 +68,14 @@ describe('POST /api/auth/register', () => {
     logOnFail(res, () => expect(res.body.data).toBeNull());
     logOnFail(res, () =>
       expect(res.body.message).toBe(
-        `Register failed : User with email ${payload.email} already exists`
+        `Register failed : User with email ${userData.email} already exists`
       )
     );
   });
 
   it('should return error with status code 400 and message "password is required" when password is missing', async () => {
-    const res = await request(app).post(endpoint).send({
-      name: 'Test User',
-      email: 'user@test.com',
-      phone_number: '081234567890',
-    });
+    const { password, ...invalidPayload } = payload;
+    const res = await request(app).post(endpoint).send(invalidPayload);
 
     logOnFail(res, () => expect(res.statusCode).toBe(400));
     logOnFail(res, () => expect(res.body.code).toBe(400));
@@ -89,31 +84,30 @@ describe('POST /api/auth/register', () => {
   });
 
   it('should return error with status code 400 and message "phone_number is required" when phone_number is missing', async () => {
-    const res = await request(app).post(endpoint).send({
-      name: 'Test User',
-      email: 'user@test.com',
-      password: 'password123',
-    });
+    const { phone_number, ...invalidPayload } = payload;
+    const res = await request(app).post(endpoint).send(invalidPayload);
 
     logOnFail(res, () => expect(res.statusCode).toBe(400));
     logOnFail(res, () => expect(res.body.code).toBe(400));
     logOnFail(res, () => expect(res.body.data).toBeNull());
-    logOnFail(res, () => expect(res.body.message).toBe('phone_number is required'));
+    logOnFail(res, () =>
+      expect(res.body.message).toBe('phone_number is required')
+    );
   });
+
   const notallowedField = 'unawantedField';
   it(`should return error with status code 400 and message "Invalid fields: ${notallowedField}" when unwanted field is given`, async () => {
     const res = await request(app).post(endpoint).send({
-      name: 'Test User',
-      email: 'user@test.com',
-      password: "tes",
-      phone_number: "tes",
+      ...payload,
       [notallowedField]: 'unwanted',
     });
 
     logOnFail(res, () => expect(res.statusCode).toBe(400));
     logOnFail(res, () => expect(res.body.code).toBe(400));
     logOnFail(res, () => expect(res.body.data).toBeNull());
-    logOnFail(res, () => expect(res.body.message).toBe(`Invalid fields: ${notallowedField}`));
+    logOnFail(res, () =>
+      expect(res.body.message).toBe(`Invalid fields: ${notallowedField}`)
+    );
   });
 });
 describe('POST /api/auth/login', () => {
@@ -126,10 +120,52 @@ describe('POST /api/auth/login', () => {
       throw err;
     }
   };
-  it('should return success with status code 200 and message "Login success" and user will get access token and refresh token when all fields are valid', async () => {
+
+  const userSeed = new UserSeeder(1);
+  const [userData] = userSeed.userSeedData.data;
+
+  const payload = {
+    email: userData.email,
+    password: userData.password,
+  };
+
+  beforeAll(async () => {
+    await userSeed.Up();
+  });
+
+  afterAll(async () => {
+    await userSeed.Down();
+  });
+
+  it('should return success with status code 200 and message "Login success" and user will get access token and refresh token when using email and password', async () => {
+    const res = await request(app).post(endpoint).send(payload);
+
+    if (res.statusCode !== 200) {
+      console.error('\n Unexpected response:');
+      console.error(JSON.stringify(res.body, null, 2));
+    }
+
+    logOnFail(res, () => expect(res.body.code).toBe(200));
+    logOnFail(res, () => expect(res.body).toHaveProperty('data'));
+    logOnFail(res, () =>
+      expect(res.body.data).toHaveProperty('access_token')
+    );
+    logOnFail(res, () =>
+      expect(res.body.data.access_token.expiry).toBe('5m')
+    );
+    logOnFail(res, () =>
+      expect(res.body.data).toHaveProperty('refresh_token')
+    );
+    logOnFail(res, () =>
+      expect(res.body.data.refresh_token.expiry).toBe('1d')
+    );
+    logOnFail(res, () => expect(res.body.message).toBe('Login success'));
+  });
+
+  it('should return success with status code 200 and message "Login success" and user will get access token and refresh token when using name and password', async () => {
     const res = await request(app).post(endpoint).send({
-      email: 'user@test.com',
-      password: 'password123',
+      name: userData.name,
+      password: userData.password,
     });
 
     if (res.statusCode !== 200) {
@@ -139,48 +175,58 @@ describe('POST /api/auth/login', () => {
 
     logOnFail(res, () => expect(res.body.code).toBe(200));
     logOnFail(res, () => expect(res.body).toHaveProperty('data'));
-    logOnFail(res, () => expect(res.body.data).toHaveProperty('access_token'));
-    logOnFail(res, () => expect(res.body.data.access_token.expiry).toBe('5m'));
-    logOnFail(res, () => expect(res.body.data).toHaveProperty('refresh_token'));
-    logOnFail(res, () => expect(res.body.data.refresh_token.expiry).toBe('1d'));
+    logOnFail(res, () =>
+      expect(res.body.data).toHaveProperty('access_token')
+    );
+    logOnFail(res, () =>
+      expect(res.body.data.access_token.expiry).toBe('5m')
+    );
+    logOnFail(res, () =>
+      expect(res.body.data).toHaveProperty('refresh_token')
+    );
+    logOnFail(res, () =>
+      expect(res.body.data.refresh_token.expiry).toBe('1d')
+    );
     logOnFail(res, () => expect(res.body.message).toBe('Login success'));
   });
 
-  it('should return error with status code 400 and message "Either name or email is required" when email or name is missing', async () => {
-    const res = await request(app).post(endpoint).send({
-      password: 'password123',
-    });
+  it('should return error with status code 400 and message "Either name or email is required" when email and name are missing', async () => {
+    const { email, name, ...invalidPayload } = payload;
+    const res = await request(app).post(endpoint).send(invalidPayload);
 
     logOnFail(res, () => expect(res.statusCode).toBe(400));
     logOnFail(res, () => expect(res.body.code).toBe(400));
     logOnFail(res, () => expect(res.body.data).toBeNull());
-    logOnFail(res, () => expect(res.body.message).toBe('Either name or email is required'));
+    logOnFail(res, () =>
+      expect(res.body.message).toBe('Either name or email is required')
+    );
   });
+
   it('should return error with status code 400 and message "password is required" when password is missing', async () => {
-    const res = await request(app).post(endpoint).send({
-      name: 'Test User',
-      email: 'user@test.com',
-    });
+    const { password, ...invalidPayload } = payload;
+    const res = await request(app).post(endpoint).send(invalidPayload);
 
     logOnFail(res, () => expect(res.statusCode).toBe(400));
     logOnFail(res, () => expect(res.body.code).toBe(400));
     logOnFail(res, () => expect(res.body.data).toBeNull());
-    logOnFail(res, () => expect(res.body.message).toBe('password is required'));
+    logOnFail(res, () =>
+      expect(res.body.message).toBe('password is required')
+    );
   });
 
   const notallowedField = 'unawantedField';
   it(`should return error with status code 400 and message "Invalid fields: ${notallowedField}" when unwanted field is given`, async () => {
     const res = await request(app).post(endpoint).send({
-      name: 'Test User',
-      email: 'user@test.com',
-      password: "tes",
+      ...payload,
       [notallowedField]: 'unwanted',
     });
 
     logOnFail(res, () => expect(res.statusCode).toBe(400));
     logOnFail(res, () => expect(res.body.code).toBe(400));
     logOnFail(res, () => expect(res.body.data).toBeNull());
-    logOnFail(res, () => expect(res.body.message).toBe(`Invalid fields: ${notallowedField}`));
+    logOnFail(res, () =>
+      expect(res.body.message).toBe(`Invalid fields: ${notallowedField}`)
+    );
   });
 });
 describe('POST Logout', () => {
@@ -195,13 +241,23 @@ describe('POST Logout', () => {
     }
   };
 
-  beforeAll(async () => {
-    const loginRes = await request(app).post('/api/auth/login').send({
-      email: 'user@test.com',
-      password: 'password123',
-    });
+  const userSeed = new UserSeeder(1);
+  const [userData] = userSeed.userSeedData.data;
 
+  const payload = {
+    email: userData.email,
+    password: userData.password,
+  };
+
+  beforeAll(async () => {
+    await userSeed.Up();
+    const loginRes = await request(app).post('/api/auth/login').send(payload)
     accessToken = loginRes.body.data.access_token.token;
+
+  });
+
+  afterAll(async () => {
+    await userSeed.Down();
   });
 
   it('should return success with status code 200 and message "Logout success" when accessToken is valid', async () => {
@@ -257,13 +313,24 @@ describe('POST GenerateToken', () => {
     }
   };
 
-  beforeAll(async () => {
-    const loginRes = await request(app).post('/api/auth/login').send({
-      email: 'user@test.com',
-      password: 'password123',
-    });
 
+  const userSeed = new UserSeeder(1);
+  const [userData] = userSeed.userSeedData.data;
+
+  const payload = {
+    email: userData.email,
+    password: userData.password,
+  };
+
+  beforeAll(async () => {
+    await userSeed.Up();
+    const loginRes = await request(app).post('/api/auth/login').send(payload)
     refreshToken = loginRes.body.data.refresh_token.token;
+
+  });
+
+  afterAll(async () => {
+    await userSeed.Down();
   });
 
   it('should return success with status code 200 and message "GenerateToken success" and user will get new access token and refresh token when refreshToken is valid', async () => {
@@ -321,7 +388,6 @@ describe('CORS Headers on Auth Endpoints', () => {
   endpoints.forEach(({ method, url }) => {
     it(`should return Access-Control-Allow-Origin * header for ${method.toUpperCase()} ${url}`, async () => {
       const res = await request(app)[method](url);
-
       expect(res.headers).toHaveProperty('access-control-allow-origin');
       expect(res.headers['access-control-allow-origin']).toBe('*');
     });
